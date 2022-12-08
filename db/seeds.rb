7d9fd4feb3640a7b7cfd7de247d0c2de3fb9c7e7
Brendan McBride <brendan.mcmath@gmail.com>
2022-12-08 18:43:22 +0800

    add api call to seed file and refactor the homepage + movie index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
-Movie.create(title: 'Wonder Woman 1984',
-             synopsis: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s', url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', rating: 6.9)
-Movie.create(title: 'The Shawshank Redemption',
-             synopsis: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison', url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg', rating: 8.7)
-Movie.create(title: 'Titanic',
-             synopsis: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9)
-Movie.create(title: "Ocean's Eight",
-             synopsis: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0)
+require 'open-uri'
+require 'json'
+
+popular_url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=88bc341d3392f9386fddd1d57bc85b70'
+
+serialized_response = URI.open(popular_url).read
+response = JSON.parse(serialized_response)
+
+puts 'Request complete, adding movies'
+response['results'].each do |result|
+  movie = Movie.new
+  movie.title = result['title']
+  movie.synopsis = result['overview']
+  movie.url = "https://image.tmdb.org/t/p/w500#{result['poster_path']}"
+  movie.rating = result['vote_average']
+  puts movie
+  movie.save
+end
+
+puts 'Seeding complete'
